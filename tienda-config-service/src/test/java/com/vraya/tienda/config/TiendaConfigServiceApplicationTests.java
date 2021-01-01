c667f04a39fe43288d10493997aa3e731d9450df
Lucye <lethiciass.r@gmail.com>
2021-01-01 14:12:39 -0600

    Subí config server

---
+++ tienda-config-service/src/test/java/com/vraya/tienda/config/TiendaConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vraya.tienda.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
